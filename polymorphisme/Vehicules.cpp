b23f780ed6fb3eb596de94969448763b08695d0b
sle-lieg <sle-lieg@sle-liegs-Mac.local>
2018-02-05 00:04:52 +0100

    polymorphisme class garage

--- polymorphisme/Vehicules.cpp
+++ polymorphisme/Vehicules.cpp
@@ -5,11 +5,29 @@
 using namespace std;
 
 /**
+ ** Class GARAGE
+ **/
+Garage::Garage()
+{}
+
+void    Garage::ajouterVehicule(Vehicule *newVehicule)
+{
+    _stock.push_back(newVehicule);
+}
+
+void    Garage::AfficherStock() const
+{
+    for (int i = 0; i < _stock.size(); i++)
+    {
+        _stock[i]->affiche();
+    }
+}
+
+/**
  ** Class VEHICULE
  **/
-Vehicule::Vehicule(string marque, int prix) : _marque(marque), _prix(prix)
-{
-}
+Vehicule::Vehicule(string marque, int prix, int annee) : _marque(marque), _prix(prix), _annee(annee)
+{}
 
 Vehicule::~Vehicule()
 {}
@@ -17,14 +35,21 @@ Vehicule::~Vehicule()
 void    Vehicule::affiche() const
 {
     cout << "---- VEHICULE ----" << endl;
+    getInfos();
+}
+
+void    Vehicule::getInfos() const
+{
+    cout << "   Marque          :" << _marque << endl;
+    cout << "   Prix            :" << _prix << endl;
+    cout << "   Annee           :" << _annee << endl;
 }
 
 /**
  ** Class VOITURE
  **/
-Voiture::Voiture(string marque, int prix, int nbPortes) : Vehicule(marque, prix), _nbPortes(nbPortes)
-{
-}
+Voiture::Voiture(string marque, int prix, int annee, int nbPortes) : Vehicule(marque, prix, annee), _nbPortes(nbPortes)
+{}
 
 Voiture::~Voiture()
 {}
@@ -32,14 +57,20 @@ Voiture::~Voiture()
 void    Voiture::affiche() const
 {
     cout << "---- VOITURE ----" << endl;
+    getInfos();
+}
+
+void    Voiture::getInfos() const
+{
+    Vehicule::getInfos();
+    cout << "   Nombre de portes:" << _nbPortes << endl;    
 }
 
 /**
  ** Class MOTO
  **/
-Moto::Moto(std::string marque, int prix, double vitesse) : Vehicule(marque, prix), _vitesse(vitesse)
-{
-}
+Moto::Moto(string marque, int prix, int annee, double vitesse) : Vehicule(marque, prix, annee), _vitesse(vitesse)
+{}
 
 Moto::~Moto()
 {}
@@ -47,4 +78,32 @@ Moto::~Moto()
 void    Moto::affiche() const
 {
     cout << "---- MOTO ----" << endl;
+    getInfos();
+}
+
+void    Moto::getInfos() const
+{
+    Vehicule::getInfos();
+    cout << "   Vitesse max     :" << _vitesse << endl;    
+}
+
+/**
+ ** Class CAMION
+ **/
+Camion::Camion(string marque, int prix, int annee, int poid) : Vehicule(marque, prix, annee), _poid(poid)
+{}
+
+Camion::~Camion()
+{}
+
+void    Camion::affiche() const
+{
+    cout << "---- CAMION ----" << endl;
+    getInfos();
+}
+
+void    Camion::getInfos() const
+{
+    Vehicule::getInfos();
+    cout << "   Poids           :" << _poid << endl;    
 }
