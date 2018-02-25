1c2b0d22f2c46995fe05f07768f6de858422a726
Stephane LE LIEGARD <sle-lieg@e2r8p20.42.fr>
2018-02-25 18:29:55 +0100

    d00 ex01

--- piscine_C++/d00/ex01/phonebook.hpp
+++ piscine_C++/d00/ex01/phonebook.hpp
@@ -8,31 +8,43 @@
 class Contact {
     public:
         Contact();
-        void    displayTruncContact(int index);
-        void    displayFullContact();        
+        void    displayTruncContact(int index) const;
+        void    displayFullContact() const;
+        void    displayData(std::string data) const;
+
         void    setFirstName(std::string data);
         void    setLastName(std::string data);
-
+        void    setNickName(std::string data);
+        void    setLogin(std::string data);
+        void    setPostalAddress(std::string data);
+        void    setEmailAddress(std::string data);
+        void    setPhoneNumber(std::string data);
+        void    setBirthDate(std::string data);
+        void    setFavMeal(std::string data);
+        void    setUnderwearColor(std::string data);
+        void    setDarkestSecret(std::string data);
+        
     protected:
         std::string _firstName;
         std::string _lastName;
         std::string _nickName;
-        // std::string login;
-        // std::string postalAddress;
-        // std::string emailAddress;
-        // std::string phoneNUmber;
-        // std::string birthDate;
-        // std::string favoriteMeal;
-        // std::string underwearColor;
-        // std::string darkestSecret;
+        std::string _login;
+        std::string _postalAddress;
+        std::string _emailAddress;
+        std::string _phoneNUmber;
+        std::string _birthDate;
+        std::string _favoriteMeal;
+        std::string _underwearColor;
+        std::string _darkestSecret;
 };
 
 class Phonebook {
     public:
         Phonebook();
         void    add();
-        void    search();
-        void    exit();
+        void    search() const;
+        void    exit() const;
+        void    displayContactsList() const;
 
     protected:
         int     _nbContacts;
